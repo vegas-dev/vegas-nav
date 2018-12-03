cca2e53187cdc629fde4fb3c1c30a9feb9a1679f
Alexander Vetlugin <vetlugins@gmail.com>
2018-12-03 23:16:24 +0300

    js

--- dist/js/jquery.vegasMegaMenu.js
+++ dist/js/jquery.vegasMegaMenu.js
@@ -1,3 +1,27 @@
 /**
- * Created by vetlu on 03.12.2018.
+ * Created by Vegas s on 03.12.2018.
  */
+
+(function( $ ) {
+	"use strict";
+
+	$.fn.vegasMegaMenu = function(options) {
+
+		options = $.extend({
+
+		}, arguments[0] || {});
+
+		var $self = this;
+
+		$(document).on('click', 'li.dropdown a', function () {
+			var $_self = $(this);
+
+			$_self.parent('li').toggleClass('show');
+
+			return false;
+		});
+
+
+		return false;
+	};
+})(jQuery);
