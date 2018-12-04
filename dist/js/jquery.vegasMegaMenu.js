196508e6d3d8433bf9b461cd5f9e4618d91e96d8
Alexander Vetlugin <vetlugins@gmail.com>
2018-12-04 17:23:21 +0300

    fix

--- dist/js/jquery.vegasMegaMenu.js
+++ dist/js/jquery.vegasMegaMenu.js
@@ -11,16 +11,53 @@
 
 		}, arguments[0] || {});
 
-		var $self = this;
+		var $self = this,
+			mainClass = 'vg-menu-main-content',
+			show = 'show';
 
-		$(document).on('click', 'li.dropdown a', function () {
+		$self.addClass(mainClass);
+
+		$(document).on('click', 'li a', function () {
 			var $_self = $(this);
+			var $li = $_self.parent('li');
 
-			$_self.parent('li').toggleClass('show');
+			if ($li.hasClass('dropdown')) {
+				if($li.parent('ul').hasClass(mainClass)) {
+					var $lvl = $self.find('.'+ show);
+					if($lvl.hasClass('current')) {
+						$lvl.removeClass(show);
+					}
 
-			return false;
+					if(!$li.hasClass('current')) {
+						$li.addClass(show).addClass('current');
+						$lvl.removeClass('current');
+					} else {
+						$li.removeClass(show).removeClass('current');
+					}
+				} else {
+					if ($li.hasClass('show')) {
+						$_self.parent('li').removeClass('show');
+						if ($li.parent('ul').hasClass(mainClass)) {
+							$self.find('.show').removeClass('show');
+						}
+					} else {
+						$_self.parent('li').addClass('show');
+					}
+				}
+
+				return false;
+			} else {
+				$self.find('.'+ show).removeClass(show).removeClass('current');
+				return $_self;
+			}
 		});
 
+		$(document).mouseup(function (e) {
+			var container = $('.' + mainClass);
+			if (container.has(e.target).length === 0){
+				$self.find('.'+ show).removeClass(show).removeClass('current');
+			}
+		});
 
 		return false;
 	};
