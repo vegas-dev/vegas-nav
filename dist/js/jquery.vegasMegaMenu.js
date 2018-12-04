219def9192043bf1d4086576360149f44c49f6b0
Alexander Vetlugin <vetlugins@gmail.com>
2018-12-04 20:04:22 +0300

    fixed

--- dist/js/jquery.vegasMegaMenu.js
+++ dist/js/jquery.vegasMegaMenu.js
@@ -17,44 +17,39 @@
 
 		$self.addClass(mainClass);
 
-		$(document).on('click', 'li:not(.dropdown-mega) a', function () {
+		$(document).on('click', 'li.dropdown a', function () {
 			var $_self = $(this);
 			var $li = $_self.parent('li');
 
 			$('.dropdown-mega').removeClass('show');
 
-			if ($li.hasClass('dropdown')) {
-				if($li.parent('ul').hasClass(mainClass)) {
-					var $lvl = $self.find('.'+ show);
-					if($lvl.hasClass('current')) {
-						$lvl.removeClass(show);
-					}
-
-					if(!$li.hasClass('current')) {
-						$li.addClass(show).addClass('current');
-						$lvl.removeClass('current');
-					} else {
-						$li.removeClass(show).removeClass('current');
-					}
-				} else {
-					if ($li.hasClass('show')) {
-						$_self.parent('li').removeClass('show');
-						if ($li.parent('ul').hasClass(mainClass)) {
-							$self.find('.show').removeClass('show');
-						}
-					} else {
-						$_self.parent('li').addClass('show');
-					}
+			if($li.parent('ul').hasClass(mainClass)) {
+				var $lvl = $self.find('.'+ show);
+				if($lvl.hasClass('current')) {
+					$lvl.removeClass(show);
 				}
 
-				return false;
+				if(!$li.hasClass('current')) {
+					$li.addClass(show).addClass('current');
+					$lvl.removeClass('current');
+				} else {
+					$li.removeClass(show).removeClass('current');
+				}
 			} else {
-				$self.find('.'+ show).removeClass(show).removeClass('current');
-				return $_self;
+				if ($li.hasClass('show')) {
+					$_self.parent('li').removeClass('show');
+					if ($li.parent('ul').hasClass(mainClass)) {
+						$self.find('.show').removeClass('show');
+					}
+				} else {
+					$_self.parent('li').addClass('show');
+				}
 			}
+
+			return false;
 		});
 
-		$(document).on('click', 'li.dropdown-mega a', function () {
+		$(document).on('click', 'li.dropdown-mega > a', function () {
 			var $_self = $(this);
 			var $li = $_self.parent('li');
 
